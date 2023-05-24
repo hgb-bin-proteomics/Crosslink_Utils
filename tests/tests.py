542b1acb079ed5dccf6a91b398572f4f28483084
Micha Birklbauer <micha.birklbauer@gmail.com>
2023-05-24 16:56:45 +0200

    update tests

--- tests/tests.py
+++ tests/tests.py
@@ -12,5 +12,6 @@ def test_find_shortest1():
     from find_shortest import find_shortest
 
     shortest = find_shortest("pyXlinkViewer_export.csv", [["C", "D"]])
+    shorest["ConstraintSatisfied"] = shortest["CA distance"].apply(lambda x: float(x) <= 27.0)
 
     assert shortest.shape[0] == 17 and shortest[shortest["ConstraintSatisfied"]].shape[0] == 9
