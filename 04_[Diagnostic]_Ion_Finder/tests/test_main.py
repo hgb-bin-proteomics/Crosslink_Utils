49b1ad5d460f472fe2c685dcd4be83eb4e369ff6
Micha Birklbauer <19354640+michabirklbauer@users.noreply.github.com>
2026-01-08 13:34:42 +0100

    add tests

---
+++ 04_[Diagnostic]_Ion_Finder/tests/test_main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+def test1():
+    from count_matched_spectra import main
+
+    result = main(argv=["-s", "data/XLpeplib_Beveridge_QEx-HFX_DSS_R1.mgf", "-m", "data/masses.txt"])
+    assert len(result) == 7
+    assert 215 in result.values()
+    assert 741 in result.values()
+    assert 137 in result.values()
+    assert 1295 in result.values()
+    assert 364 in result.values()
+    assert 133 in result.values()
+    assert 189 in result.values()
